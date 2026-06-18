65f366e340e40db7f8e99acf61a0e0ea6722ccd6
Bacary Thienty <bacarythienty803@gmail.com>
2026-06-18 10:06:18 +0000

    connexion

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ dotenv.config()
 const app = express();
 connectBD();
 app.use(express.json());
-app.use(cors({ origin: [
-'http://localhost:3000', 'https://front-node-sigma.vercel.app'
-]}));
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 const PORT = process.env.PORT;
 app.listen( PORT , () => {
     console.log(`serveur démarré sur http://localhost:${PORT}` );
